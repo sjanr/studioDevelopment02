dddc949244e2c1474c0b1d3422ffa0c09fcfc5f2
sjanr <97484144+sjanr@users.noreply.github.com>
2025-02-09 14:49:55 -0800

    Modified ball to move along with player

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,25 +5,25 @@ public class BallController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
-    [SerializeField] private float force = 10f;
+    [SerializeField] private float force = 1f;
+    [SerializeField] private Transform ballAnchor;
     [SerializeField] private InputManager inputManager;
     private bool isBallLaunched;
     private Rigidbody ballRB;
-    void Start()
-    {
-        // Grabbing a reference to RigidBody
+    void Start() {
         ballRB = GetComponent<Rigidbody>();
-
-        // Add a listener to the OnSpacePressed event.
-        // When the space key is pressed the
-        // LaunchBall method will be called.
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
         
     }
 
     private void LaunchBall() {
         if (isBallLaunched) return;
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
 
     }
