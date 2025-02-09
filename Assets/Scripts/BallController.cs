c0ada67c3091de2490fa16b3e9bed4aa685c4c7b
sjanr <97484144+sjanr@users.noreply.github.com>
2025-02-08 22:08:30 -0800

    Modified BallController to disable multiple clicks on spacebar

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,8 +4,10 @@ using UnityEngine.Events;
 public class BallController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
+    private bool isBallLaunched;
     private Rigidbody ballRB;
     void Start()
     {
@@ -20,7 +22,10 @@ public class BallController : MonoBehaviour
     }
 
     private void LaunchBall() {
+        if (isBallLaunched) return;
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+
     }
 
     // Update is called once per frame
